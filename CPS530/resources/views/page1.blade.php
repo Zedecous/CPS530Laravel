8c136f823a65192ebeedfacbfc47d2fcb0ad1919
Zedecous <swatenya@gmail.com>
2019-11-30 13:02:58 +0000

    Installed semantic

--- CPS530/resources/views/page1.blade.php
+++ CPS530/resources/views/page1.blade.php
@@ -2,12 +2,15 @@
 <html>
 	<head>
 		<meta charset="UTF-8">
+		<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.css"> 
+		<script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.js"></script>
 	</head>
 	<body>
 		<title>page 1</title>
 		<p>Page 1. A summary snapshot presentation of the chosen frontend Framework: Popularity, strengths, weaknesses, which developers mostly use it and for what kind of websites. A summary snapshot presentation of the chosen backend Framework: Popularity, strengths, weaknesses, which developers mostly use it and for what kind of websites.</p>
 		<form action="{{ route('installation')}}" method="get">
-				<input type="submit" value="Go to page 2" />
+				<input type="submit" class="ui submit basic button" value="Go to page 2" />
 		</form>
 		<form action="{{ route('tutorial')}}" method="get">
 			<input type="submit" value="Go to page 3" />
